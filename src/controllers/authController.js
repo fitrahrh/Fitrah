c60bc78790976483758185eddcea1a6ce4b3ea14
fitrahrh <rh.fitrah@gmail.com>
2023-12-29 22:34:24 +0700

    update

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,35 +1,12 @@
-const User = require('../models/User');
+// controllers/authController.js
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
-const login = async (req, res) => {
-  const { email, password } = req.body;
-
-  try {
-    // Cari pengguna berdasarkan email
-    const user = await User.findOne({ email });
-
-    // Jika pengguna tidak ditemukan
-    if (!user) {
-      return res.status(401).json({ message: 'Email atau password salah' });
-    }
-
-    // Verifikasi kata sandi
-    const passwordMatch = await bcrypt.compare(password, user.password);
-
-    // Jika kata sandi tidak sesuai
-    if (!passwordMatch) {
-      return res.status(401).json({ message: 'Email atau password salah' });
-    }
-
-    // Buat token JWT
-    const token = jwt.sign({ userId: user._id }, 'rahasia', { expiresIn: '1h' });
-
-    // Kirim pesan validasi
-    res.status(200).json({ message: 'Login berhasil', token });
-  } catch (error) {
-    res.status(500).json({ message: 'Error dalam proses login', error });
-  }
+exports.register = async (req, res) => {
+  // Implement registration logic
 };
 
-module.exports = { login };
+exports.login = async (req, res) => {
+  // Implement login logic
+};
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,9 @@
+// routes/authRoutes.js
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-// Endpoint untuk login
+router.post('/register', authController.register);
 router.post('/login', authController.login);
 
 module.exports = router;
